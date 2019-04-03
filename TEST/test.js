60963eb800ee8de1c2d5d2e5736c8e45746adefa
Trương Duyên <truongmyduyen2197@gmail.com>
2019-04-03 17:29:41 +0700

    file

---
+++ TEST/test.js
@@ -0,0 +1,7 @@
+var func=new Function("x","y","return x*y");
+function test(){
+	var pro;
+	pro=func(4,6);
+	console.log(pro)
+}
+test()
---
+++ TEST/test1.js
@@ -0,0 +1,6 @@
+function fun1(...params){
+	console.log(params.length);
+}
+fun1();
+fun1(3,5,6,5);
+fun1(v);
